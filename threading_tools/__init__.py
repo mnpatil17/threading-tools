8d08435459a6cce5c8bab15029cbdfecc5aaf23e
Mihir Patil <mihir.patil@berkeley.edu>
2017-08-06 00:33:49 -0700

    Added process decorator

--- threading_tools/__init__.py
+++ threading_tools/__init__.py
@@ -1,3 +1,3 @@
 from synchronized_number import SynchronizedNumber
 from lock_acquisition_exception import LockAcquisitionException
-from threading_decorators import threaded_fn
+from threading_decorators import threaded_fn, process_fn
--- threading_tools/threading_decorators.py
+++ threading_tools/threading_decorators.py
@@ -1,4 +1,5 @@
 import threading
+import multiprocessing
 
 
 def threaded_fn(func):
@@ -9,4 +10,15 @@ def threaded_fn(func):
         thread = threading.Thread(target=func, args=args, kwargs=kwargs)
         thread.start()
         return thread
+    return wrapper
+
+
+def process_fn(func):
+    """
+    A decorator for any function that needs to be run on a separate process
+    """
+    def wrapper(*args, **kwargs):
+        process = multiprocessing.Process(target=func, args=args, kwargs=kwargs)
+        process.start()
+        return process
     return wrapper
